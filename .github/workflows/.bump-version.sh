7f1872e42e26e1170c0089de71de52ae0c8b3a23
MRegra <marregsil97@gmail.com>
2025-07-30 20:15:09 +0200

    feat: Updating pom.xml version too

---
+++ .github/workflows/.bump-version.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Usage: ./bump-version.sh 1.2.3
+
+VERSION=$1
+
+if [[ -z "$VERSION" ]]; then
+  echo "No version passed to bump-version.sh"
+  exit 1
+fi
+
+# Update the pom.xml version tag
+echo "Bumping pom.xml to version $VERSION"
+sed -i "s|<version>.*</version>|<version>$VERSION</version>|" pom.xml
+
+# Save version to VERSION file too
+echo "$VERSION" > VERSION
