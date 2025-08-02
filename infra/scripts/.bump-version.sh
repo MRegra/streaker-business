30ae11bf600e5c21ca018f472d8f57b114e7c41a
MRegra <marregsil97@gmail.com>
2025-08-02 22:46:13 +0200

    fix: Fixing the bump version

--- infra/scripts/.bump-version.sh
+++ infra/scripts/.bump-version.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# Usage: ./bump-version.sh <version> <component>
+# Usage: ./bump-version.sh <version> <frontend|backend|global>
 
 VERSION=$1
 COMPONENT=$2
 
 if [[ -z "$VERSION" || -z "$COMPONENT" ]]; then
-  echo "Usage: ./bump-version.sh <version> <frontend|backend>"
+  echo "Usage: ./bump-version.sh <version> <frontend|backend|global>"
   exit 1
 fi
 
@@ -18,9 +18,15 @@ if [[ "$COMPONENT" == "backend" ]]; then
     exit 1
   fi
   echo "$VERSION" > VERSION_BACKEND
+
 elif [[ "$COMPONENT" == "frontend" ]]; then
   echo "Bumping frontend version to $VERSION"
   echo "$VERSION" > VERSION_FRONTEND
+
+elif [[ "$COMPONENT" == "global" ]]; then
+  echo "Bumping global version to $VERSION"
+  echo "$VERSION" > VERSION
+
 else
   echo "Unknown component: $COMPONENT"
   exit 1
